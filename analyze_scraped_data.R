3ecf9fafef36d731824c3660a713e881db713669
connoryen <connor.yen@berkeley.edu>
2024-01-31 21:30:45 -0800

    streamlined selenium solution

--- analyze_scraped_data.R
+++ analyze_scraped_data.R
@@ -1,10 +1,16 @@
 library(tidyverse)
 
-df <- read.csv('scraped_data.csv')
+df0 <- read.csv('scraped_data.csv')
+
+nrow(df0)
+
+df <- df0 %>%
+  filter(old_price != 0 & new_price != 0,
+         grepl("50 | 16 | M | L | 10 | 10.5 | ONESIZE | 40 | 34", sizes) | sizes == "") %>%
+  mutate(per_off = 1-new_price/old_price) %>%
+  arrange(-per_off)
 
 df %>%
-  filter(grepl("33", sizes),
-         new_price <= 150) %>%
-  arrange(-per_off) %>%
-  select(-category) %>%
+  select(-link) %>%
   view()
+
